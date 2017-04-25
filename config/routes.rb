73084017d052b0088b4826e6a77d26333b5c6bb8
Abdullahi Muhammed <shinamuh@yahoo.com>
2017-04-25 13:14:12 +0100

    generate pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/blog'
+
+  get 'static_pages/plans'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'application#hello'
 
